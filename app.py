05ef65edceb1432d54801170ec0a6a0b4e65ed47
Jordan <jordan@notablelabs.com>
2016-07-20 10:46:04 -0700

    finish part 1 of tutorial

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+# print(os.environ['APP_SETTINGS']) sanity check business 
 
 
 @app.route('/')
@@ -17,3 +18,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+
